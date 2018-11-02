942a934e1baebd395e86417f973694c3282b5cda
Daniel Schaffer <dan@dandoes.net>
2018-11-02 09:07:01 -0700

    string trailing periods for the s3 bucket name; add public-read acl

--- main.tf
+++ main.tf
@@ -11,7 +11,8 @@ resource "aws_route53_record" "301" {
 }
 
 resource "aws_s3_bucket" "301" {
-  bucket = "${var.name}"
+  bucket = "${substring(var.name, -2, -1) == "." ? substring(var.name, 0, length(var.name) - 1) : var.name}"
+  acl    = "public-read"
 
   website {
     redirect_all_requests_to = "${var.target}"
